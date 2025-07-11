624c910e6ea32612d0adce2e2ae19c667519dc01
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-11 20:49:39 +0530

    add rescards

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,83 @@ import React from "react";
 
 const App = () => {
   const Header = () => (
-    <div className="border h-20 flex">
+    <div className="border h-24 flex">
       <img
         src="https://static.vecteezy.com/system/resources/previews/035/526/567/original/restaurant-logo-free-vector.jpg"
-        className="h-16 "
+        className="h-22 "
       />
-      <div className="space-x-5 m-9 ml-auto">
-        <a href="">Home</a>
-        <a href="">About Us</a>
-        <a href="">Contact Us</a>
-        <a href="">Cart</a>
+      <div className="space-x-5 m-9 ml-auto ">
+        <a href="/">Home</a>
+        <a href="/">About Us</a>
+        <a href="/">Contact Us</a>
+        <a href="/">Cart</a>
       </div>
     </div>
   );
 
-  const Body = () => <div></div>;
+  const Restcard = () => (
+    <div className=" m-2 border w-46  hover:border-2 rounded ">
+      <div className="">
+        <img
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/28/68413230-9dc2-459a-91ac-c3950de16632_233050.JPG"
+          className="object-cover w-full h-full"
+        />
+        <h1>Mughal Hotel</h1>
+        <h2>Biryani,North Indian</h2>
+        <h3>4.8 Stars</h3>
+        <h3>15 minutes</h3>
+      </div>
+    </div>
+  );
+
+  const Body = () => (
+    <div className="m-3">
+      <div className="">
+        <h1>Search</h1>
+      </div>
+      <div className="flex flex-wrap">
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+        <Restcard />
+      </div>
+    </div>
+  );
 
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 };
