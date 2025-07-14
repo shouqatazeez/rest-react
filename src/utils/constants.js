6b317731a861ef94fa1457176bef5475deae5cdf
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-14 12:05:31 +0530

    add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const Cdn_Url = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const Logo_Url ="https://static.vecteezy.com/system/resources/previews/035/526/567/original/restaurant-logo-free-vector.jpg";
