dd29a0c72fd2c74860f9bb6df09ea75e35abcba2
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-15 00:49:20 +0530

    add swiggy live apis

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,35 @@
 import Restcard from "./Restcard";
-import resObj from "../utils/mockData";
-import { useState } from "react";
+// import resObj from "../utils/mockData";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [reslist, setresList] = useState(resObj);
+  const [reslist, setresList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    const restaurantCard = json.data.cards.find(
+      (card) =>
+        card?.card?.card?.gridElements?.infoWithStyle?.restaurants !== undefined
+    );
+
+    setresList(
+      restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
+    );
+
+    // setresList(
+    //   json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    // );
+  };
 
   return (
     <div className="m-3 ">
@@ -12,7 +38,7 @@ const Body = () => {
           className="border rounded cursor-pointer p-1.5 ml-2 bg-gray-200 hover:bg-gray-300"
           onClick={() => {
             const filterdata = reslist.filter(
-              (res) => res.info.avgRating > 4.5
+              (res) => res.info.avgRating > 4.3
             );
             setresList(filterdata);
           }}
