508146467e778323f9453d8718047b3805dbe629
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-14 18:21:54 +0530

    add top rated button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,28 @@
 import Restcard from "./Restcard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+  const [reslist, setresList] = useState(resObj);
+
   return (
-    <div className="m-3">
-      <div className="">
-        <h1>Search</h1>
+    <div className="m-3 ">
+      <div>
+        <button
+          className="border rounded cursor-pointer p-1.5 ml-2 bg-gray-200 hover:bg-gray-300"
+          onClick={() => {
+            const filterdata = reslist.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setresList(filterdata);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
+
       <div className="flex flex-wrap">
-        {resObj.map((restaurant, id) => (
+        {reslist.map((restaurant, id) => (
           <Restcard key={id} resData={restaurant} />
         ))}
       </div>
