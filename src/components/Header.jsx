92a44ec823d8ec36a36b7a6e12fd533c064961ce
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-16 13:03:49 +0530

    add some minor changes in login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ const Header = () => {
   const [btnName, setbtnName] = useState("Login");
   return (
     <div className="border h-24 flex">
-      <img src={Logo_Url} className="h-22 " />
+      <img src={Logo_Url} className="h-22 rounded-full" />
       <div className="space-x-5 m-9 ml-auto ">
         <a href="/">Home</a>
         <a href="/">About Us</a>
         <a href="/">Contact Us</a>
         <a href="/">Cart</a>
         <button
-          className="border rounded p-1 cursor-pointer hover:bg-gray-300 "
+          className="border rounded p-1.5 w-1xs cursor-pointer bg-gray-300 hover:bg-gray-200  "
           onClick={() => {
             btnName == "Login" ? setbtnName("Logout") : setbtnName("Login");
           }}
