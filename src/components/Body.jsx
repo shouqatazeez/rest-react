a9c827361f1a3b72170aca6d3a23241be97d7460
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-16 16:23:57 +0530

    some modifications in search

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,8 @@ import ShimmerGallery from "./ShimmerGallery";
 
 const Body = () => {
   const [reslist, setresList] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [searchtext, setsearchText] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -22,9 +24,11 @@ const Body = () => {
         card?.card?.card?.gridElements?.infoWithStyle?.restaurants !== undefined
     );
 
-    setresList(
-      restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
-    );
+    const restaurant =
+      restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
+      [];
+    setresList(restaurant);
+    setAllRestaurants(restaurant);
 
     // setresList(
     //   json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
@@ -36,12 +40,35 @@ const Body = () => {
   }
 
   return (
-    <div className="m-3 ml-12 ">
-      <div>
+    <div className="m-3 ml-12  ">
+      <div className="flex ">
+        <div>
+          <input
+            type="text "
+            placeholder="Enter restaurant name"
+            className="border  p-1 ml-2 rounded "
+            value={searchtext}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            className="border rounded ml-1 p-1 cursor-pointer hover:bg-gray-300 bg-green-200 "
+            onClick={() => {
+              const filterResdata = allRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchtext.toLowerCase())
+              );
+              setresList(filterResdata);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
         <button
-          className="border rounded cursor-pointer p-1.5 ml-2 bg-gray-200 hover:bg-gray-300"
+          className="border rounded cursor-pointer p-1.5 ml-3 bg-gray-200 hover:bg-gray-300"
           onClick={() => {
-            const filterdata = reslist.filter(
+            const filterdata = allRestaurants.filter(
               (res) => res.info.avgRating > 4.3
             );
             setresList(filterdata);
