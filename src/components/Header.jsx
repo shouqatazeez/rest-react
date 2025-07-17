792e807c7dc7a0668ab58dd4605157270fab9b94
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-17 21:25:38 +0530

    add the link to the nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { Logo_Url } from "../utils/constants";
+import { Link } from "react-router-dom";
+import About from "./About";
 const Header = () => {
   const [btnName, setbtnName] = useState("Login");
   return (
     <div className="border h-24 flex">
       <img src={Logo_Url} className="h-22 rounded-full" />
       <div className="space-x-5 m-9 ml-auto ">
-        <a href="/">Home</a>
-        <a href="/">About Us</a>
-        <a href="/">Contact Us</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact Us</Link>
+
         <a href="/">Cart</a>
         <button
           className="border rounded p-1.5 w-1xs cursor-pointer bg-gray-300 hover:bg-gray-200  "
