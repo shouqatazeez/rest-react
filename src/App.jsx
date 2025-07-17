31a0d5e27c3186df76c1212d8bca927dc0f1d2aa
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-17 17:03:56 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -11,4 +14,23 @@ const App = () => {
   );
 };
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+const Root = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default Root;
