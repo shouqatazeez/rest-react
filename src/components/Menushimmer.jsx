a96b38a3f205dd0657d09a32944a10aa6e28ee0b
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-18 21:03:30 +0530

    add menucard shimmer

---
+++ src/components/Menushimmer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ShimmerTable } from "react-shimmer-effects";
+
+const Menushimmer = () => {
+  return <ShimmerTable row={8} col={4} />;
+};
+
+export default Menushimmer;
