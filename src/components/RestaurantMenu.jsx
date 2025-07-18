a3df0e5c08e52a232e3a2e0d8efb172499ece73c
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-18 20:29:19 +0530

    add swiggy live menu apis

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const RestaurantMenu = () => {
+  const [resInfo, setresInfo] = useState();
+  useEffect(() => {
+    fetchMenuInfo();
+  }, []);
+
+  const fetchMenuInfo = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.38430&lng=78.45830&restaurantId=378344&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+    console.log(json);
+
+    setresInfo(json.data);
+  };
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
     </div>
   );
 };
