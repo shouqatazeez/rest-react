a60507ee5547182f353904da95ece727b90a44cf
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-19 20:06:32 +0530

    add Menu_Url  in constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const Cdn_Url = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const Logo_Url ="https://static.vecteezy.com/system/resources/previews/035/526/567/original/restaurant-logo-free-vector.jpg";
+
+export const Menu_Url ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/";
