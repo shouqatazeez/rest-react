f7ffd86209ff2b18097bbe61e7207644bf461f05
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-19 15:49:28 +0530

    add restaurant menu details

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import Menushimmer from "./Menushimmer";
+import { Star, MapPin } from "lucide-react";
 
 const RestaurantMenu = () => {
-  const [resInfo, setresInfo] = useState();
+  const [resInfo, setresInfo] = useState(null);
   useEffect(() => {
     fetchMenuInfo();
   }, []);
@@ -15,9 +17,40 @@ const RestaurantMenu = () => {
 
     setresInfo(json.data);
   };
+  const {
+    name,
+    avgRating,
+    totalRatingsString,
+    costForTwoMessage,
+    cuisines,
+    locality,
+  } = resInfo?.cards[2]?.card?.card?.info || {};
+
+  const { slaString } = resInfo?.cards[2]?.card?.card?.info?.sla || {};
+
+  if (resInfo == null) {
+    return <Menushimmer />;
+  }
   return (
-    <div>
-      <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
+    <div className="text-center m-2 ">
+      <h1 className="font-bold text-xl mb-3 text-gray-800">{name}</h1>
+      <div className="max-w-md mx-auto my-1 p-1 bg-white rounded-lg shadow-lg">
+        <div className="flex justify-center items-center text-sm gap-1">
+          <Star size={16} className="fill-yellow-400 text-yellow-500" />
+          <span className="font-bold text-yellow-600">{avgRating}</span>
+          <span className="text-shadow-black-400">
+            ({totalRatingsString}) - {costForTwoMessage}
+          </span>
+        </div>
+        <h2 className="m-2 text-orange-700">{cuisines.join(", ")}</h2>
+        <div className="flex justify-center items-center gap-1 text-sm text-emerald-600">
+          <MapPin size={16} className="text-emerald-600" />
+          <span>{locality}</span>
+        </div>
+        <div className="flex justify-center gap-1 text-gray-700 text-sm">
+          Delivery in<span className="font-semibold">{slaString}</span>
+        </div>
+      </div>
     </div>
   );
 };
