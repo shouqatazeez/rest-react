04ca54a0d0890d240f82dc5adc56407f4c937d12
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-24 23:56:26 +0530

    some minor modifications in rescard

--- src/components/Restcard.jsx
+++ src/components/Restcard.jsx
@@ -4,11 +4,16 @@ const Restcard = (props) => {
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
     resData.info;
   return (
-    <div className=" m-2 border w-46  hover:border-2 rounded bg-gray-200">
-      <div className="">
-        <img src={Cdn_Url + cloudinaryImageId} />
+    <div className=" m-4 p-4 border w-[200px]  hover:border-2 rounded bg-gray-200">
+      <div className=" ">
+        <img src={Cdn_Url + cloudinaryImageId} className="rounded-lg" />
 
-        <h1>{name}</h1>
+        <h1
+          className="
+        font-bold  py-3"
+        >
+          {name}{" "}
+        </h1>
         <h2>{cuisines.join(", ")}</h2>
         <h3>{avgRating} stars</h3>
         <h3>{costForTwo}</h3>
